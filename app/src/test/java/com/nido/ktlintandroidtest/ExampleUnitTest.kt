877bd22412cf916153a779eae9f5667a1eb1b7f0
naveedahmad <sr.naveed99@gmail.com>
2021-10-12 02:06:23 +0500

    ktlint Changes

--- app/src/test/java/com/nido/ktlintandroidtest/ExampleUnitTest.kt
+++ app/src/test/java/com/nido/ktlintandroidtest/ExampleUnitTest.kt
@@ -1,9 +1,6 @@
 package com.nido.ktlintandroidtest
-
 import org.junit.Test
-
 import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
